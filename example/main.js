e7ace2ccd4adc6fb0a98d0b091946c4bd3f669cf
LilPacy <revivedtomorrow@gmail.com>
2018-07-23 17:49:50 +0900

    形だけのReducerを定義

--- example/main.js
+++ example/main.js
@@ -12,3 +12,16 @@ const store = '';
 const actionSetURL = () => store.dispatch({ type: 'SET_URL' });
 const actionGetImg = () => store.dispatch({ type: 'GET_IMG' });
 const actionClear = () => store.dispatch({ type: 'CLEAR' });
+
+const reducerURL = (state = '', action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+const reducerImg = (state = '', action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}

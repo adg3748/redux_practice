70b40c13bdd5807ff3ec2c96e17ab9b5b36e89c4
LilPacy <revivedtomorrow@gmail.com>
2018-07-31 21:14:35 +0900

    renderItemが定義より上で呼び出されていたのを修正

--- reactnative-redux/app/components/home.js
+++ reactnative-redux/app/components/home.js
@@ -21,17 +21,28 @@ class Home extends Component {
     }; // this.stateは空だがthis.props.getDataにはApp.jsから渡されたstoreが格納されている
   }
   componentDidMount(){
+    this.props.getData();
   }
+  renderItem = ({item,index}) => (
+    <View style={styles.row}>
+      <Text style={styles.title}>
+      </Text>
+      <Text style={styles.description}>
+          {item.description}
+      </Text>
+    </View>
+  );
   render() {
+    console.log(this.props,this.state);
     if (this.props.loading){
       return (
-        <View style={{backgroundColor: '#000000'}}>
-          <ActivityIndicator />
+        <View style={{backgroundColor: '#333'}}>
+          <ActivityIndicator animating={true}/>
         </View>
       );
     } else {
       return (
-        <View style={{flex:1,backgroundColor: '#FF0000',paddingTop:20}}>
+        <View style={{flex:1,backgroundColor: '#aaa',paddingTop:20}}>
           <Text>aaa</Text>
           <FlatList
             ref='listRef'
@@ -41,23 +52,14 @@ class Home extends Component {
         </View>
       );
     }
-    renderItem = ({item,index}) => {
-      <View style={styles.row}>
-        <Text style={styles.title}>
-        </Text>
-        <Text style={styles.description}>
-            {item.description}
-        </Text>
-      </View>
-   }
   }
 };
 
 function mapStateToProps(state,props) { // stateにはreturnに記述したオブジェクトに加えてcombineReducersに記述したreducerが入っている
   return {
-    loading: false,
-    data: ''
-  };
+    loading: state.dataReducer.loading,
+    data: state.dataReducer.data
+  }
 }
 
 function mapDispatchToProps(dispatch) {
@@ -65,6 +67,9 @@ function mapDispatchToProps(dispatch) {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
+// 第1引数はcomponentに渡すpropsの制御
+// 第2引数はreducerを呼び出してreduxで管理しているstateを更新する
+// 2つ目の()は取得したデータをpropsとして扱いたいcomponentを指定する
 
 const styles = StyleSheet.create({
   activityIndicatorContainer:{
